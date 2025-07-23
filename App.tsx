0ba0a836b34cf274932f53fd7bc4f6eb0d3eeda2
Mateus Queiroz <mateusqueirozdev@gmail.com>
2025-07-23 15:48:51 -0300

    chore: add hure icons

--- App.tsx
+++ App.tsx
@@ -1,12 +1,29 @@
 import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import { SearchIcon } from '@hugeicons/core-free-icons';
+import { HugeiconsIcon } from '@hugeicons/react-native';
 
 import { fonts, typography } from '@/theme';
 
 function App() {
   return (
     <SafeAreaView style={styles.container}>
-        <Text style={{ fontFamily: fonts.bold, fontSize: typography.bold.lg }}>Marketplace App</Text>
-        <Text style={{ fontFamily: fonts.medium, fontSize: typography.medium.label.md }}>Seu app de compras</Text>
+      <Text style={{ fontFamily: fonts.bold, fontSize: typography.bold.lg }}>
+        Marketplace App
+      </Text>
+      <Text
+        style={{
+          fontFamily: fonts.medium,
+          fontSize: typography.medium.label.md,
+        }}
+      >
+        Seu app de compras
+      </Text>
+      <HugeiconsIcon
+        icon={SearchIcon}
+        size={24}
+        color="black"
+        strokeWidth={1.5}
+      />
     </SafeAreaView>
   );
 }
